77b21d6170e2ab594f8813d042df5aef32a7ee0d
e3Dr8nj <c44.c4@yandex.ru>
2021-10-12 01:43:14 +0300

    'firstpartreactcomp'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent.js'
 import {Navbar,NavbarBrand} from 'reactstrap'
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Navbar dark color ="primary">
          <div className ="container">
            <NavbarBrand href='/'>Ristorane2</NavbarBrand>
          </div>
     </Navbar>
+    <Menu />
     </div>
   );
 }
