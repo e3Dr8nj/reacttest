997be41d6c4066db7934d86d487f0f64c068c5c0
e3Dr8nj <c44.c4@yandex.ru>
2021-11-17 23:04:17 +0300

    'dishRender'

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,16 +4,37 @@ class Menu extends Component{
     constructor(props){
         super(props)
         this.state={
-            dishes:[ 
+            selectedDish : null
+        /*    dishes:[ 
                 {id:'1',name:'dish1',description:'very tasty'}
                 ,{id:'2',name:'dish2',description:'very tasty2'}
             ]
+            */
         }
+        
+    }
+    onDishSelect=(dish)=>{
+      this.setState({selectedDish:dish})
+        
+    }
+    dishRender=(dish)=>{
+        
+        if(dish!=null){
+           
+            return(
+                <Card>
+                    <CardImg src = {dish.image}></CardImg>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )}else{return (<div></div>)}
     }
     render(props){
         const menu = this.props.dishes.map(dish=>{return(
         <div className = "col-12 col-md-5 m-1" id={dish.id}>
-             <Card key = {dish.id}>
+             <Card onClick={()=>this.onDishSelect(dish)} key = {dish.id}>
                  <CardImg width="100%" src = {dish.image} alt = {dish.name}></CardImg>
                  <CardImgOverlay>
                      <CardTitle>{dish.name}</CardTitle>
@@ -23,8 +44,14 @@ class Menu extends Component{
         return(
             <div className = "component">
             <div className ="row">
-               
+                    
                     {menu}
+              
+            </div>
+            <div className ="row" >
+            <div  className="col-12 col-md-5 m-1">
+                    {this.dishRender(this.state.selectedDish)}
+            </div>
               
             </div>
             </div>
