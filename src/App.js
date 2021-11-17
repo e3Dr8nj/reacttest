159621b7bf83f33dd8c54c5cddf44d6b6758a231
e3Dr8nj <c44.c4@yandex.ru>
2021-11-17 23:21:24 +0300

    'render+'

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import './App.css';
 import Menu from './components/MenuComponent.js'
 import {Navbar,NavbarBrand} from 'reactstrap'
 import DISHES from './shared/dishes'
-function App() {
-  return (
-    <div >
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  
+    
+    render(props){return(<div>
       <Navbar dark color ="primary">
          <div className ="container">
            <NavbarBrand href='/'>Ristorane2</NavbarBrand>
          </div>
     </Navbar>
-    <Menu dishes={DISHES} />
-    </div>
-  );
+    <Menu dishes={this.state.dishes} ></Menu>
+    </div>)}
+    
+    
 }
 
 export default App;
