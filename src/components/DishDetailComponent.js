97b058e21e037f9e4742f18eca9492e8570f44ba
e3Dr8nj <c44.c4@yandex.ru>
2021-11-20 00:32:17 +0300

    'task2+'

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,12 +3,23 @@ import  {Media,Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 're
 
 import React, { Component } from 'react'
 class DishDetail extends Component{
-    /*
+    
         constructor(props){
             super(props)
             this.state={}
         }
-        */
+        renderDish(dish){
+            return(
+                <Card>
+                    <CardImg width="100%" src = {dish.image}></CardImg>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        }
+    
        render(props){
           if(this.props.dish==null) return(<div></div>)
            return(
@@ -16,13 +27,7 @@ class DishDetail extends Component{
                 <div className="row">
                      <div  className="col-12 col-md-5 m-1">
                          
-                     <Card>
-                    <CardImg width="100%" src = {this.props.dish.image}></CardImg>
-                    <CardBody>
-                        <CardTitle>{this.props.dish.name}</CardTitle>
-                        <CardText>{this.props.dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                     {this.renderDish(this.props.dish)}
                 
             </div>
            <div  className="col-12 col-md-5 m-1">hhh</div>
